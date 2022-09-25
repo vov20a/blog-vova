a0d0cdeed47a34145d62ef0bc9fd241b04b9cfde
Petr <vov20a@mail.ru>
2022-09-25 20:34:38 +0300

    REACT_APP_API_URL

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -57,7 +57,7 @@ export const FullPost = () => {
         id={data._id}
         title={data.title}
         // imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-        imageUrl={data.imageUrl ? `http://localhost:4444${data.imageUrl}` : ''}
+        imageUrl={data.imageUrl ? `${process.env.REACT_APP_API_URL}${data.imageUrl}` : ''}
         // user={{
         //   avatarUrl:
         //     'https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png',
